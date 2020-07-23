d5547cb8956f3f984b292a1bf8bc32b03aa99933
Andrey Kudryavtsev <lomastering@gmail.com>
2020-07-23 17:37:46 +0300

    feat: background changer

---
+++ README.md
@@ -0,0 +1,14 @@
+# CatLogic Clock
+**Self-study CSS & JavaScript Project**
+
+### About
+
+A pure CSS clock displaying current time from the user machine.
+* Current live version at http://catlogic.ru/catlogic-clock/
+
+### Under the hood
+
+* Designed by pure CSS shadows and gradients.
+* A simple script for fetching current user time, changing background color and for some decorative use.
+* JavaScript ES6 syntax, CSS3 + semantic HTML5
+* HTML and CSS naming by BEM.
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
+const longHand = document.querySelector('.clock__long-hand');
+const longShadowHand = document.querySelector('.clock__long-shadow-hand');
+const shortHand = document.querySelector('.clock__short-hand');
+const shortShadowHand = document.querySelector('.clock__short-shadow-hand');
+const body = document.querySelector('.body');
+const colorChoosers = Array.from(document.querySelectorAll('.colorChooser__item'));
+
 const addStrokes = () => {
   const strokes = document.querySelectorAll('.stroke');
   let deg = 6;
@@ -7,22 +14,24 @@ const addStrokes = () => {
   });
 };
 
-const longHand = document.querySelector('.clock__long-hand');
-const longShadowHand = document.querySelector('.clock__long-shadow-hand');
-const shortHand = document.querySelector('.clock__short-hand');
-const shortShadowHand = document.querySelector('.clock__short-shadow-hand');
-
 const setDate = () => {
   const currentTime = new Date();
-  const seconds = currentTime.getSeconds();
   const minutes = currentTime.getMinutes();
   const hours = currentTime.getHours();
-  longHand.style.animationDelay = `${-(minutes * 60 + seconds)}s`;
-  longShadowHand.style.animationDelay = `${-(minutes * 60 + seconds)}s`;
-  shortHand.style.animationDelay = `${-(hours * 60 * 60 + minutes * 60 + seconds)}s`;
-  shortShadowHand.style.animationDelay = `${-(hours * 60 * 60 + minutes * 60 + seconds)}s`;
-  console.log(hours, minutes);
+  longHand.style.transform = `rotate(${minutes / 60 * 360 - 180}deg)`;
+  longShadowHand.style.transform = `rotate(${minutes / 60 * 360 - 180}deg)`;
+  shortHand.style.transform = `rotate(${hours / 12 * 360 - 180 + 30 * minutes / 60}deg)`;
+  shortShadowHand.style.transform = `rotate(${hours / 12 * 360 - 180 + 30 * minutes / 60}deg)`;
 };
+
+const setColor = (event) => {
+  body.style.backgroundColor = event.target.dataset.color;
+}
 
 addStrokes();
 setDate();
+setInterval(setDate, 1000);
+
+colorChoosers.forEach(chooser => {
+  chooser.addEventListener('click', setColor);
+});
